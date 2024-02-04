18e7049d60ed94e8dbd227de217cddaa36c3936a
xledz <leonidaspinheiro1@gmail.com>
2024-02-03 21:28:34 -0300

    implementa endpoints para inserção de pesagem dos animais

--- src/main/java/com/gado/gerenciamento/dominio/converters/PesagemAnimalConverter.java
+++ src/main/java/com/gado/gerenciamento/dominio/converters/PesagemAnimalConverter.java
@@ -1,15 +1,17 @@
 package com.gado.gerenciamento.dominio.converters;
 
 import com.gado.gerenciamento.controller.pesagem.dto.PesagemAnimalDto;
+import com.gado.gerenciamento.dominio.model.Animal;
 import com.gado.gerenciamento.dominio.model.PesagemAnimal;
 
 public class PesagemAnimalConverter {
 
-    public static PesagemAnimal fromDtoToEntity(PesagemAnimalDto dto) {
+    public static PesagemAnimal fromDtoToEntity(PesagemAnimalDto dto, Animal animal) {
         PesagemAnimal pesagem = new PesagemAnimal();
 
         pesagem.setPeso(dto.peso());
         pesagem.setDataPesagem(dto.dataPesagem());
+        pesagem.setAnimalId(animal);
 
         return pesagem;
     }
--- src/main/java/com/gado/gerenciamento/dominio/service/PesagemService.java
+++ src/main/java/com/gado/gerenciamento/dominio/service/PesagemService.java
@@ -2,25 +2,34 @@ package com.gado.gerenciamento.dominio.service;
 
 import com.gado.gerenciamento.controller.pesagem.dto.PesagemAnimalDto;
 import com.gado.gerenciamento.dominio.converters.PesagemAnimalConverter;
+import com.gado.gerenciamento.dominio.model.Animal;
 import com.gado.gerenciamento.dominio.model.PesagemAnimal;
+import com.gado.gerenciamento.dominio.repository.AnimalRepository;
 import com.gado.gerenciamento.dominio.repository.PesagemAnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PesagemService {
     @Autowired
     private PesagemAnimalRepository pesagemAnimalRepository;
+    @Autowired
+    private AnimalRepository animalRepository;
 
     public List<PesagemAnimal> listAllPesagemAnimais() {
         return pesagemAnimalRepository.findAll();
     }
 
     public PesagemAnimal savePesagemAnimal(PesagemAnimalDto data) {
-        PesagemAnimal NovaPesagem = PesagemAnimalConverter.fromDtoToEntity(data);
-        return pesagemAnimalRepository.save(NovaPesagem);
+        Optional<Animal> animal = animalRepository.findById(data.animalId());
+
+        //TODO implement error handling in case of animal being not found
+
+        PesagemAnimal novaPesagem = PesagemAnimalConverter.fromDtoToEntity(data, animal.get());
+        return pesagemAnimalRepository.save(novaPesagem);
     }
 
 }
